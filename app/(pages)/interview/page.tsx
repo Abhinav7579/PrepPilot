3147065e3e19ee51a5af417067466700906d81aa
Abhinav <abhinavsyunary@gmail.com>
2025-07-07 01:18:32 +0530

    vapi integration

--- app/(pages)/interview/page.tsx
+++ app/(pages)/interview/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { getCurrentUser } from '@/lib/actions/auth.action'
 import Agent from '@/components/agent'
-const page = () => {
+const page = async() => {
+  const user=await getCurrentUser();
   return (
     <>
     <h3>Interview Generation</h3>
-    <Agent userName="You" userId="user1" type="generate"/>
+    <Agent userName={user?.name} userId={user?.id} type="generate"/>
       
     </>
   )
