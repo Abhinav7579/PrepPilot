f5bfcd86daca77050e05ba109a4e18003d0f125f
Abhinav <abhinavsyunary@gmail.com>
2025-07-23 16:21:00 +0530

    loader changed

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen w-screen bg-transparent">
+    <div className="flex items-center justify-center  bg-transparent">
       <div className="w-12 h-12 border-4 border-blue-700 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
